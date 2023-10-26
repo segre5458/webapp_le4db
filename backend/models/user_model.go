d6c82657d95f4bd0e5eea8657222541fbfa61688
segre <segrehouck5458@gmail.com>
2023-10-26 13:45:10 +0900

    add model

--- backend/models/user_model.go
+++ backend/models/user_model.go
@@ -1,6 +1,48 @@
 package models
 
-type TestUser struct {
-    UserID   int
-    Password string
+type Rack struct {
+    height int
+    unit_number int
+    width int
+    specification int
+    depth int
+}
+
+type Server struct {
+    server_name string
+    ip_address string
+    os string
+    cpu string
+    memory int
+    storage int
+    nic_mac_address string
+}
+
+type ServerPlacement struct {
+    nic_mac_address string
+    rack_unit_number int
+}
+
+type NetworkDevice struct {
+    device_name string
+    port_count int
+    ip_address []string
+    role string
+    port_mac_address []string
+}
+
+type DCUser struct {
+    uid string
+    name string
+}
+
+type Table struct {
+    uid string
+    server_or_network_device_name string
+}
+
+type Connection struct {
+    rack_number int
+    note string
+    server_or_network_device_name string
 }
--- database/init.sql
+++ database/init.sql
@@ -1,8 +1,57 @@
-CREATE TABLE TEST_USER (
-    user_id BIGINT PRIMARY KEY,
-    user_password VARCHAR(20) NOT NULL
+CREATE TABLE rack (
+    height integer NOT NULL,
+    unit_number integer NOT NULL CHECK (unit_number >= 1 AND unit_number <= 100),
+    width integer NOT NULL,
+    specification integer CHECK (specification IN (19, 21, NULL)),
+    depth integer NOT NULL
 );
 
-DELETE FROM TEST_USER;
-INSERT INTO TEST_USER VALUES(1, 'password1');
-INSERT INTO TEST_USER VALUES(2, 'password2');
+CREATE TABLE server (
+    server_name character varying NOT NULL,
+    ip_address character varying NOT NULL,
+    os character varying NOT NULL,
+    cpu character varying NOT NULL,
+    memory integer NOT NULL,
+    storage integer NOT NULL,
+    nic_mac_address character varying NOT NULL
+);
+
+CREATE TABLE server_placement (
+    nic_mac_address character varying NOT NULL,
+    rack_unit_number integer NOT NULL
+);
+
+ALTER TABLE server_placement
+    ADD CONSTRAINT placement_unique_unit_number UNIQUE (rack_unit_number);
+
+CREATE TABLE network_device (
+    device_name character varying NOT NULL,
+    port_count integer NOT NULL,
+    ip_addresses inet[] NOT NULL,
+    role character varying NOT NULL,
+    port_mac_addresses character varying[] NOT NULL
+);
+
+CREATE TABLE network_device_placement (
+    port_mac_address character varying NOT NULL,
+    rack_unit_number integer NOT NULL
+);
+
+ALTER TABLE network_device_placement
+    ADD CONSTRAINT placement_unique_unit_number UNIQUE (rack_unit_number);
+
+CREATE TABLE DCuser (
+    uid character varying NOT NULL,
+    name character varying NOT NULL
+);
+
+CREATE TABLE charge (
+    uid character varying NOT NULL,
+    server_or_network_device_name character varying NOT NULL
+);
+
+CREATE TABLE connection (
+    rack_number integer NOT NULL,
+    note character varying NOT NULL,
+    server_or_network_device_name character varying NOT NULL
+);
