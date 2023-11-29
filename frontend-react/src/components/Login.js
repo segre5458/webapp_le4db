7e03729299cf266423587da1eb4278ec2f963c05
segre <segrehouck5458@gmail.com>
2023-11-29 09:32:06 +0900

    add react

---
+++ frontend-react/src/components/Login.js
@@ -0,0 +1,55 @@
+import React, { useState } from "react";
+
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+
+    if (!username || !password) {
+      setError("Username and password are required.");
+      return;
+    }
+
+    setError("");
+    console.log("Login successful!");
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      {error && <div style={{ color: "red" }}>{error}</div>}
+      <form onSubmit={handleLogin}>
+        <div>
+          <label htmlFor="username">Username:</label>
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <div>
+          <button type="submit">Login</button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
